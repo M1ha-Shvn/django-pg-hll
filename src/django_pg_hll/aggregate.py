2fe7192d2136cf015e416076ca158a9c49eae45e
M1ha <work_shvein_mihail@mail.ru>
2019-10-08 13:09:54 +0500

    Added CardinalitySum function

--- src/django_pg_hll/aggregate.py
+++ src/django_pg_hll/aggregate.py
@@ -13,11 +13,21 @@ class UnionAgg(Aggregate):
     output_field = HllField()
 
 
-class UnionAggCardinality(UnionAgg):
+class UnionAggCardinality(Aggregate):
     """
     I haven't found a way to combine function inside function in django.
     So, I've written function to get aggregate cardinality with one call
     """
     function = 'hll_union_agg'
     template = 'hll_cardinality(%(function)s(%(expressions)s))'
+    output_field = IntegerField()
+
+
+class CardinalitySum(Aggregate):
+    """
+    I haven't found a way to combine function inside function in django.
+    So, I've written function to get sum cardinality with one call
+    """
+    function = 'hll_cardinality'
+    template = 'SUM(%(function)s(%(expressions)s))'
     output_field = IntegerField()
